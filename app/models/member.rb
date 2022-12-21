681534b726b961ddafe75ad8d0c0831a163db195
Muzaffar <mus.ariff@gmail.com>
2022-12-21 22:54:53 +0800

    remove member numbers temporary

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,9 +3,11 @@ class Member < ApplicationRecord
    has_many :loans, dependent: :destroy
 
    validates :name, presence: true
-   validates :ic_number, 
-              presence: true, uniqueness: true, 
-              format: { with: /\d{2}(0[1-9]|1[012])(0[1-9]|[12][0-9]|3[01])/, message: "Format No Kad Pengenalan salah" }
+   validates :ic_number, presence: true, uniqueness: true, numericality: { only_integer: true}
+   
+   # validates :ic_number,
+   #            presence: true, uniqueness: true,
+   #            format: { with: /\d{2}(0[1-9]|1[012])(0[1-9]|[12][0-9]|3[01])/, message: "Format No Kad Pengenalan salah" }
    validates :number, presence: true, uniqueness: true
 
    scope :ordered, -> { order(name: :asc) }
